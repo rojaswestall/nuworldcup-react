e6bb9aec0abdfdb6594100a3baec31b6f6543e63
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-04-28 09:33:37 -0500

    gonzo page

--- nuwc-react/src/components/Gonzo.js
+++ nuwc-react/src/components/Gonzo.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import title from '../assets/img/titles/gonzo-title.png';
+import infographic from '../assets/img/gonzo/gonzo-infograph-short.png';
+import Footer from './Footer';
 
 class Gonzo extends Component {
     render() {
 		return (
-			<div> GONZO </div>
+			<div>
+				<div className="d-flex justify-content-center title-div">
+            		<img className="title" src={title} alt={"GONZO"}/> 
+        		</div>
+
+        		<div className="d-flex justify-content-center infographic-div">
+            		<img className="infographic" src={infographic} alt={"Infographic"}/> 
+        		</div>
+
+				<Footer />
+			</div>
 	    );
 	}
 }
