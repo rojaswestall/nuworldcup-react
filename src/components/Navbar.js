6a7b89990bee7a78e649aeece098b87a946ddc10
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-11-03 11:54:43 -0500

    adding registration link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,9 @@ class Navbar extends Component {
 						</ul>
 						<ul className="navbar-nav">
 							<li className="nav-item">
+								<a className="nav-link" href="https://docs.google.com/forms/d/e/1FAIpQLSc52zYiXGeRShcX3movdpE5wZfwYNfkJYi7t574pc1K-8MnOw/viewform"> Register </a>
+							</li>
+							<li className="nav-item">
 								<Link className="nav-link" to={'/FAQ'}> FAQ </Link>
 							</li>
 							
